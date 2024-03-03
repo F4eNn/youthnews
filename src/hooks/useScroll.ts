d92a7cdd2d7851e4da272608d39b8c3694132f12
Mateusz <mateusz4k@outlook.com>
2024-03-03 13:27:42 +0100

    created useScrollHook

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,22 +1,22 @@
 import { useEffect, useState, useRef } from 'react';
 
 export const useScroll = () => {
-   const [isScroll, setIsScroll] = useState({ isScrollUp: true, isScrollDown: false });
-   const lastScrollPosition = useRef(0);
+   const [isScrollUp, setIsScrollUp] = useState(false);
+   const lastScrollPostion = useRef(0);
 
    useEffect(() => {
       const handleScroll = () => {
          const currentScrollPosition = window.scrollY;
 
-         if (lastScrollPosition.current > currentScrollPosition) {
-            setIsScroll({ isScrollUp: true, isScrollDown: false });
-         } else setIsScroll({ isScrollDown: true, isScrollUp: false });
-         lastScrollPosition.current = currentScrollPosition;
+         if (lastScrollPostion.current > currentScrollPosition) {
+            setIsScrollUp(true);
+         } else setIsScrollUp(false);
+         lastScrollPostion.current = currentScrollPosition;
       };
       window.addEventListener('scroll', handleScroll);
 
       return () => window.removeEventListener('scroll', handleScroll);
    }, []);
 
-   return { ...isScroll, lastScrollPosition };
+   return isScrollUp;
 };
